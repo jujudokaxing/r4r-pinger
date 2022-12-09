f30bc94d08099bd3aa697e18a24b3f7dc04f149e
Ryan <119848808+jujudokaxing@users.noreply.github.com>
2022-12-09 23:32:01 +0800

    Update show_post.py

--- show_post.py
+++ show_post.py
@@ -1,6 +1,7 @@
 import praw
 import ctypes  # An included library with Python install.   
 import sys
+from tkinter import *
 
 def msgb0x(text, title):
 	ctypes.windll.user32.MessageBoxW(0, text, title, 0)
@@ -8,18 +9,24 @@ def msgb0x(text, title):
 g = sys.argv # submission id not fixed
 subm_id = g[-1]
 
-
 # login to reddit
-reddit = praw.Reddit(client_id = 'qd3fFRnBK468ud50B-D-lA',
-					client_secret = 'wXZctO0d2rQb2fujr2pnschuAPN3aA', 
-					username = 'DempseyCallz',
-					password = 'Mountaindew!1',
-					user_agent = 'NANANA',)
+reddit = praw.Reddit(client_id = '',
+					client_secret = '', 
+					username = '',
+					password = '',
+					user_agent = '',)
 
 post_red = reddit.submission(id=subm_id)
 
-content = str(post_red.title) + '\n\n' + str(post_red.selftext)
+content = str(post_red.title) + '\nBy: ' + str(post_red.author) + '\n\n' + str(post_red.selftext)
 a = '[F4M] FOUND!'
 b = content
 
-msgb0x(str(b), str(a))
+# new message box, bigger
+msg = Message(text=b)
+msg.config(bg='white', font=('Calibri', 16))
+msg.pack()
+mainloop()
+
+# old message box, smaller
+# msgb0x(str(b), str(a))
